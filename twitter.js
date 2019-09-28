f006c06f5e269ce9c2820e4152e483e8427a7b7b
Maya Mubayi <mayamubayi@Mayas-MacBook-Pro.local>
2019-09-28 15:39:19 -0400

    init file commit

---
+++ twitter.js
@@ -0,0 +1,158 @@
+var tweet =
+{
+      "text": "you asked and we listened! our delicious whole milk vanilla & plain skyr are now available without the lactose! find our lactose free tubs nearest you! #dailysiggis",
+      "truncated": true,
+      "in_reply_to_user_id": null,
+      "in_reply_to_status_id": null,
+      "favorited": false,
+      "source": "<a href=\"http://twitter.com/\" rel=\"nofollow\">Twitter for iPhone</a>",
+      "in_reply_to_screen_name": null,
+      "in_reply_to_status_id_str": null,
+      "id_str": "54691802283900928",
+      "entities": {
+            "user_mentions": [
+                  {
+                        "indices": [
+                              3,
+                              19
+                        ],
+                        "screen_name": "siggi's",
+                        "id_str": "271572434",
+                        "name": "siggisdairy",
+                        "id": 271572434
+                  }
+            ],
+            "urls": [ ],
+            "hashtags": [ 
+                  {
+                        "indices": [
+                              152,
+                              164
+                            ],
+                            "text": "dailysiggis" 
+                  }
+
+            ]
+      },
+      "contributors": null,
+      "retweeted": false,
+      "in_reply_to_user_id_str": null,
+      "place": null,
+      "retweet_count": 82,
+      "favourite_count": 123,
+      "created_at": "Fri Jul 19 12:00:00 +0000 2019",
+      "retweeted_status": {
+            "text":  "", 
+            "truncated": false,
+            "in_reply_to_user_id": null,
+            "in_reply_to_status_id": null,
+            "favorited": false,
+            "source": "<a href=\"http://www.hootsuite.com\" rel=\"nofollow\">HootSuite</a>",
+            "in_reply_to_screen_name": null,
+            "in_reply_to_status_id_str": null,
+            "id_str": "54640519019642881",
+            "entities": {
+                  "user_mentions": [ ],
+                  "urls": [ ],
+                  "hashtags": [
+                        {
+                              "text": "PGP",
+                              "indices": [
+                                    130,
+                                    134
+                              ]
+                        }
+                  ]
+            },
+            "contributors": null,
+            "retweeted": false,
+            "in_reply_to_user_id_str": null,
+            "place": null,
+            "retweet_count": 62,
+            "created_at": "Sun Apr 03 20:24:49 +0000 2011",
+            "user": {
+                  "notifications": null,
+                  "profile_use_background_image": true,
+                  "statuses_count": 31,
+                  "profile_background_color": "red",
+                  "followers_count": 3066,
+                  "profile_image_url": "profile.jpg",
+                  "listed_count": 6,
+                  "profile_background_image_url": "http://a3.twimg.com/a/1301071706/images/themes/theme1/bg.png",
+                  "description": "",
+                  "screen_name": "Null",
+                  "default_profile": true,
+                  "verified": false,
+                  "time_zone": null,
+                  "profile_text_color": "333333",
+                  "is_translator": false,
+                  "profile_sidebar_fill_color": "DDEEF6",
+                  "location": "",
+                  "id_str": "271572434",
+                  "default_profile_image": false,
+                  "profile_background_tile": false,
+                  "lang": "en",
+                  "friends_count": 21,
+                  "protected": false,
+                  "favourites_count": 0,
+                  "created_at": "Thu Mar 24 19:45:44 +0000 2011",
+                  "profile_link_color": "0084B4",
+                  "name": "siggi's",
+                  "show_all_inline_media": false,
+                  "follow_request_sent": null,
+                  "geo_enabled": false,
+                  "profile_sidebar_border_color": "C0DEED",
+                  "url": null,
+                  "id": 271572434,
+                  "contributors_enabled": false,
+                  "following": null,
+                  "utc_offset": null
+            },
+            "id": 54640519019642880,
+            "coordinates": null,
+            "geo": null
+      },
+      "user": {
+            "notifications": null,
+            "profile_use_background_image": true,
+            "statuses_count": 12604,
+            "profile_background_color": "C0DEED",
+            "followers_count": 9787,
+            "profile_image_url": "siggispic.jpg",
+            "listed_count": 0,
+            "profile_background_image_url": "http://a3.twimg.com/a/1300479984/images/themes/theme1/bg.png",
+            "description": "rather delicious yogurt products with simple ingredients and not a lot of sugar",
+            "screen_name": "siggisdairy",
+            "default_profile": true,
+            "verified": false,
+            "time_zone": "Hawaii",
+            "profile_text_color": "333333",
+            "is_translator": false,
+            "profile_sidebar_fill_color": "DDEEF6",
+            "location": "The State of New York",
+            "id_str": "80177619",
+            "default_profile_image": false,
+            "profile_background_tile": false,
+            "lang": "en",
+            "friends_count": 81,
+            "protected": false,
+            "favourites_count": 0,
+            "created_at": "Tue Oct 06 01:13:17 +0000 2009",
+            "profile_link_color": "0084B4",
+            "name": "siggi's",
+            "show_all_inline_media": false,
+            "follow_request_sent": null,
+            "geo_enabled": false,
+            "profile_sidebar_border_color": "C0DEED",
+            "url": null,
+            "id": 80177619,
+            "contributors_enabled": false,
+            "following": null,
+            "utc_offset": -36000
+      },
+      "id": 54691802283900930,
+      "coordinates": null,
+      "geo": null
+};
+
+
